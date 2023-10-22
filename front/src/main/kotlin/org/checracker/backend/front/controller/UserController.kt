581411094f3cbd2a3336d0033c595cf09844872a
shinyerin <shinyerin@kidaristudio.com>
2023-10-23 01:40:31 +0900

    test commit

--- front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
+++ front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
@@ -2,19 +2,18 @@ package org.checracker.backend.front.controller
 
 import org.checracker.backend.front.model.request.UserRequest
 import org.checracker.backend.front.service.UserService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("v1/user")
 class UserController(
-    private val userService: UserService
+    private val userService: UserService,
 ) {
     @GetMapping("/test")
     fun getUsers() = userService.getUsers()
 
     @PostMapping("/test")
-    fun saveUser(request: UserRequest) = userService.saveUser(request)
+    fun saveUser(
+        @RequestBody request: UserRequest,
+    ) = userService.saveUser(request)
 }
