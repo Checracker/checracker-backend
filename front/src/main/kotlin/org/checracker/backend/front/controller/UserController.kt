ec6106c7dfeea1dac1d0d00f3e15fb642f5a89d7
effiRin <effirin8@gmail.com>
2023-10-24 01:11:03 +0900

    DB 연동 임시 로깅

--- front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
+++ front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
@@ -1,5 +1,6 @@
 package org.checracker.backend.front.controller
 
+import org.checracker.backend.core.user.entity.User
 import org.checracker.backend.front.model.request.UserRequest
 import org.checracker.backend.front.service.UserService
 import org.springframework.web.bind.annotation.*
@@ -10,7 +11,10 @@ class UserController(
     private val userService: UserService,
 ) {
     @GetMapping("/test")
-    fun getUsers() = userService.getUsers()
+    fun getUsers(): List<User> {
+        println("DB 연동 TEST")
+        return userService.getUsers()
+    }
 
     @PostMapping("/test")
     fun saveUser(
