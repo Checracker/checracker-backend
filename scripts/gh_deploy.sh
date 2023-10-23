e3cdafbe567da6d0eee9b22aa79dbff9c74e2093
effiRin <effirin8@gmail.com>
2023-10-23 23:39:44 +0900

    스크립트 수정 - jar path 및 포트번호 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -3,7 +3,8 @@
 PROJECT_NAME="github_action"
 JAR_PATH="/home/ubuntu/github_action/build/front/build/libs/*.jar"
 
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+#DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/home/ubuntu/github_action/build/front/build/libs/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
@@ -14,8 +15,8 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
-echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-cp $BUILD_JAR $DEPLOY_PATH
+#echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
+#cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -32,7 +33,8 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8081 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+#nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8081 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
