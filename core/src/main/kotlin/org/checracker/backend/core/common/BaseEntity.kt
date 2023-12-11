a842c49d67250f48f1149d6e562129448f501e58
effiRin <effirin8@gmail.com>
2023-12-12 02:04:38 +0900

    feat. BaseEntity 추가

---
+++ core/src/main/kotlin/org/checracker/backend/core/common/BaseEntity.kt
@@ -0,0 +1,20 @@
+package org.checracker.backend.core.common
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass // BaseEntity를 상속한 엔티티들은 BaseEntity에 있는 멤버변수들을 모두 컬럼으로 인식하도록 함.
+@EntityListeners(value = [AuditingEntityListener::class]) // 자동으로 값을 매핑시키겠다는 의미
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    open var createdAt: Long = System.currentTimeMillis()
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    open var updatedAt: Long = System.currentTimeMillis()
+}
