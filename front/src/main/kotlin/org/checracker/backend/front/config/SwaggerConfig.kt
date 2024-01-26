eaa72700428ca9e2b2536609139b464e2e7c8c15
effiRin <effirin8@gmail.com>
2024-01-26 22:39:52 +0900

    config: swagger jwt 토큰 설정 추가

--- front/src/main/kotlin/org/checracker/backend/front/config/SwaggerConfig.kt
+++ front/src/main/kotlin/org/checracker/backend/front/config/SwaggerConfig.kt
@@ -3,15 +3,34 @@ package org.checracker.backend.front.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
+private const val SECURITY_SCHEME_NAME = "authorization"
 
 @Configuration
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
-        .components(Components())
-        .info(apiInfo())
+        .components(
+            Components().addSecuritySchemes(
+                SECURITY_SCHEME_NAME,
+                SecurityScheme()
+                    .name(SECURITY_SCHEME_NAME)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"),
+            ),
+        )
+        .addSecurityItem(SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+        .info(
+            apiInfo()
+                .title("체크래커 API Docs")
+                .description("체크래커 API 문서입니다")
+                .version("1.0.0"),
+        )
 
     private fun apiInfo() = Info()
         .title("Springdoc 테스트")
