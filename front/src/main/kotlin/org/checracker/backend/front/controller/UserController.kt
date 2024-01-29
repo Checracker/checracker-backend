ea7953ad918834d77e17d059a45e06636006042c
effiRin <effirin8@gmail.com>
2024-01-29 22:25:35 +0900

    test 커밋

--- front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
+++ front/src/main/kotlin/org/checracker/backend/front/controller/UserController.kt
@@ -1,5 +1,6 @@
 package org.checracker.backend.front.controller
 
+import io.swagger.v3.oas.annotations.media.Schema
 import org.checracker.backend.core.entity.user.User
 import org.checracker.backend.front.service.UserService
 import org.springframework.web.bind.annotation.*
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*
 class UserController(
     private val userService: UserService,
 ) {
+    @Schema(description = "테스트용")
     @GetMapping("/test")
     fun getUsers(): List<User> {
         println("LOGGING ::: TEST")
